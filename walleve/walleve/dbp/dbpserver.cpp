5c5abac530f1bcc74d18529d048043d613e09be5
MathxH Chen <brainfvck@foxmail.com>
2018-08-21 17:08:46 +0800

    [+]: implement a part of key functions of Dbp Server

--- walleve/walleve/dbp/dbpserver.cpp
+++ walleve/walleve/dbp/dbpserver.cpp
@@ -8,33 +8,38 @@ using namespace walleve;
 
  CDbpClient::CDbpClient(CDbpServer *pServerIn,CDbpProfile *pProfileIn,
                 CIOClient* pClientIn)
+: pServer(pServerIn), pProfile(pProfileIn), pClient(pClientIn)
+  
 {
-
 }
 
 CDbpClient::~CDbpClient()
 {
-
+    if(pClient) pClient->Close();
 }
 
-CDbpProfile *GetProfile()
+CDbpProfile *CDbpClient::GetProfile()
 {
-
+    return pProfile;
 }
 
 bool CDbpClient::IsEventStream()
 {
-
+    return fEventStream;
 }
 
 void CDbpClient::SetEventStream()
 {
-
+    fEventStream = true;
 }
 
 void CDbpClient::Activate()
 {
-
+    fEventStream = false;
+    ssRecv.Clear();
+    ssSend.Clear();
+    
+    StartReadHeader();
 }
 
 void CDbpClient::SendResponse(std::string& strResponse)
@@ -85,7 +90,13 @@ CDbpServer::~CDbpServer()
 
 CIOClient* CDbpServer::CreateIOClient(CIOContainer *pContainer)
 {
-
+    std::map<boost::asio::ip::tcp::endpoint,CDbpProfile>::iterator it;
+    it = mapProfile.find(pContainer->GetServiceEndpoint());
+    if (it != mapProfile.end() && (*it).second.pSSLContext != NULL)
+    {
+        return new CSSLClient(pContainer,GetIoService(),*(*it).second.pSSLContext);
+    }
+    return CIOProc::CreateIOClient(pContainer);
 }
 
 void CDbpServer::HandleClientRecv(CDbpClient *pDbpClient,
@@ -106,27 +117,67 @@ void CDbpServer::HandleClientError(CDbpClient *pDbpClient)
 
 void CDbpServer::AddNewHost(const CDbpHostConfig& confHost)
 {
-
+    vecHostConfig.push_back(confHost);
 }
 
 bool CDbpServer::WalleveHandleInitialize()
 {
-
+    BOOST_FOREACH(const CDbpHostConfig& confHost,vecHostConfig)
+    {
+        if (!CreateProfile(confHost))
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 void CDbpServer::WalleveHandleDeinitialize()
 {
-
+    for (std::map<boost::asio::ip::tcp::endpoint,CDbpProfile>::iterator it = mapProfile.begin();
+         it != mapProfile.end(); ++it)
+    {
+        delete (*it).second.pSSLContext;
+    }
+    mapProfile.clear();
 }
 
 void CDbpServer::EnterLoop()
 {
+    WalleveLog("Dbp Server starting:\n");
+
+    for (std::map<boost::asio::ip::tcp::endpoint,CDbpProfile>::iterator it = mapProfile.begin();
+         it != mapProfile.end(); ++it)
+    {
+        if(!StartService((*it).first,(*it).second.nMaxConnections))
+        {
+             WalleveLog("Setup service %s failed, listen port = %d, connection limit %d\n",
+                    (*it).second.pIOModule->WalleveGetOwnKey().c_str(),
+                    (*it).first.port(),(*it).second.nMaxConnections);
+        }
+        else
+        {
+             WalleveLog("Setup service %s success, listen port = %d, connection limit %d\n",
+                    (*it).second.pIOModule->WalleveGetOwnKey().c_str(),
+                    (*it).first.port(),(*it).second.nMaxConnections);
+        } 
+    }
 
 }
 
 void CDbpServer::LeaveLoop()
 {
-
+    std::vector<CDbpClient *>vClient;
+    for (std::map<uint64,CDbpClient*>::iterator it = mapClient.begin();it != mapClient.end();++it)
+    {
+        vClient.push_back((*it).second);
+    }
+    
+    BOOST_FOREACH(CDbpClient *pClient,vClient)
+    {
+        RemoveClient(pClient);
+    }
+    WalleveLog("Dbp Server stop\n");
 }
 
 bool CDbpServer::ClientAccepted(const boost::asio::ip::tcp::endpoint& epService,CIOClient *pClient)
@@ -136,7 +187,38 @@ bool CDbpServer::ClientAccepted(const boost::asio::ip::tcp::endpoint& epService,
 
 bool CDbpServer::CreateProfile(const CDbpHostConfig& confHost)
 {
+    CDbpProfile profile;
+    if (!WalleveGetObject(confHost.strIOModule,profile.pIOModule))
+    {
+        WalleveLog("Failed to request %s\n",confHost.strIOModule.c_str());
+        return false;
+    }
 
+    if(confHost.optSSL.fEnable)
+    {
+        profile.pSSLContext = new boost::asio::ssl::context(boost::asio::ssl::context::sslv23);
+        if (!profile.pSSLContext)
+        {
+            WalleveLog("Failed to alloc ssl context for %s:%u\n",
+            confHost.epHost.address().to_string().c_str(),
+            confHost.epHost.port());
+            return false;
+        }
+        if (!confHost.optSSL.SetupSSLContext(*profile.pSSLContext))
+        {
+            WalleveLog("Failed to setup ssl context for %s:%u\n",
+            confHost.epHost.address().to_string().c_str(),
+            confHost.epHost.port());
+            delete profile.pSSLContext;
+            return false;
+        }
+    }
+
+    profile.nMaxConnections = confHost.nMaxConnections;
+    profile.vAllowMask = confHost.vAllowMask;
+    mapProfile[confHost.epHost] = profile;
+
+    return true;
 }
 
 CDbpClient* CDbpServer::AddNewClient(CIOClient *pClient,CDbpProfile *pDbpProfile)
