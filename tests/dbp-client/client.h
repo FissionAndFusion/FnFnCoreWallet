94f75b4b4ff3b52ae45ef2c536f4f89636abbf73
gc87 <sfme@qq.com>
2018-08-30 23:40:09 +0800

    sub msg & ping msg & test.

--- tests/dbp-client/client.h
+++ tests/dbp-client/client.h
@@ -5,6 +5,7 @@
 #include <boost/asio/steady_timer.hpp>
 #include <boost/asio/system_timer.hpp>
 #include <boost/asio/high_resolution_timer.hpp>
+#include "dbp.pb.h"
 
 class client
 {
@@ -17,7 +18,10 @@ class client
     void start();
     void conn_handler(const boost::system::error_code& ec, std::shared_ptr<boost::asio::ip::tcp::socket> sock);
     void read_handler(const boost::system::error_code &ec, std::shared_ptr<boost::asio::ip::tcp::socket> sock);
-    void timer_handler(const boost::system::error_code &ec);
+    void timer_handler(const boost::system::error_code &ec, std::shared_ptr<boost::asio::ip::tcp::socket> sock);
+    void write_handler(boost::shared_ptr<std::string> pstr, const boost::system::error_code &ec, size_t bytes_transferred);
+    dbp::Base create_msg(dbp::Msg msg, google::protobuf::Message &obj);
+    std::vector<char> serialize(dbp::Base base);
 
   private:
     boost::asio::io_service m_io;
