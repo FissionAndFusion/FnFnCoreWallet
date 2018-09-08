7b58003a65fa6b770a4f28475653cf4104b21321
Loading Sun <jhkghmn@gmail.com>
2018-09-08 22:48:48 +0800

    Remove a warning

--- src/mode/mode.h
+++ src/mode/mode.h
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 //
 //
-// Introduce functions related Mode:
+// Introduction about Mode:
 // 
 // Concept:
 //  - Mode: Program execution mode. e.g. server, console, miner, dnseed...
