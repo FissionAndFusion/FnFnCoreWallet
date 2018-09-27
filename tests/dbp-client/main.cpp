c3a6cc95673037411e01cbe421dea47019bd495b
MathxH Chen <brainfvck@foxmail.com>
2018-09-27 12:56:00 +0800

    [-]:  delete unnessary member in ThreadCxt dbpclient

--- tests/dbp-client/main.cpp
+++ tests/dbp-client/main.cpp
@@ -44,7 +44,6 @@ typedef struct ThreadCxt
     boost::asio::ip::tcp::endpoint ep;
 
     boost::asio::ip::tcp::socket g_socket;
-    std::thread::id threadId;
     std::string session_id;
 } ThreadCxt;
 
@@ -303,9 +302,9 @@ void method_func(ThreadCxt &cxt)
 
     lws::GetBlocksArg block_arg;
     uint256 hash;
-    hash.SetHex("a63d6f9d8055dc1bd7799593fb46ddc1b4e4519bd049e8eba1a0806917dcafc0");
+    hash.SetHex("06e274748ddc3f89e39f79dc758c55866d3a4f90b905d05aec85f4e147779d73");
     block_arg.set_hash(std::string(hash.begin(), hash.end()));
-    block_arg.set_number(5);
+    block_arg.set_number(1);
     google::protobuf::Any *block_any = new google::protobuf::Any();
     block_any->PackFrom(block_arg);
 
@@ -357,6 +356,18 @@ static void print_block(lws::Block &block)
     std::cout << "   hash:" << GetHex(hash) << std::endl;
     std::cout << "   height:" << block.nheight() << std::endl;
     std::cout << "   prev hash:" << GetHex(prev_hash) << std::endl;
+
+    std::cout << "vtx size: " << block.vtx_size() << std::endl;
+
+    std::cout << "vtx v input size: " << block.vtx(0).vinput_size() << std::endl;
+    for (int i = 0; i < block.vtx(0).vinput_size(); ++i)
+    {
+        std::string txhash(block.vtx(0).vinput(i).hash());
+        reverse(txhash.begin(), txhash.end());
+
+        std::cout << "InputTxHash: " << GetHex(txhash) << std::endl;
+        std::cout << "InputTx n: " << block.vtx(0).vinput(i).n() << std::endl;
+    }
 }
 
 static void print_tx(lws::Transaction &tx)
@@ -515,7 +526,6 @@ void run_state_machine(ThreadCxt &cxt)
 
 void test_client(ThreadCxt &cxt)
 {
-    cxt.threadId = std::this_thread::get_id();
     cxt.state = START;
     while (true)
     {
