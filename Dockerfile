09688cdd06ceaef487d848e6ec9ceaae2bc2fd8c
gc87 <sfme@qq.com>
2018-12-23 12:48:23 +0800

    [#]add tail -f /dev/null

--- Dockerfile
+++ Dockerfile
@@ -1,39 +1,27 @@
 FROM mysql:5.7
-# MAINTAINER gaochun@fnfn.io
+MAINTAINER gaochun@fnfn.io
 
 ENV HOME=/home/fnfn
-
-ENV DNSEED=
-ENV DNSEED_PORT=
 
 ENV DB_HOST=localhost
 ENV DB_PORT=3306
 ENV DB_NAME=multiverse
 
-ENV DBP_R_HOST=
-ENV DBP_R_PORT=6815
-ENV FORK_ID=
-
 RUN apt-get update \
-&& apt-get upgrade -y \
-&& apt-get install -y libssl1.0-dev \
-&& apt-get install -y libssl1.1 \
-#&& apt-get install -y libboost-all-dev \
-&& apt-get install -y libmysqlclient-dev \
-&& apt-get install -y libsodium-dev \
-&& apt-get install -y libreadline6-dev \
-&& apt-get install -y libreadline7 \
-&& apt-get install -y mysql-client
-#&& apt-get install -y iproute2
+    && apt-get upgrade -y \
+    && apt-get install -y libssl1.0-dev \
+    # && apt-get install -y libssl1.1 \
+    && apt-get install -y libmysqlclient-dev \
+    && apt-get install -y libsodium-dev \
+    # && apt-get install -y libreadline6-dev \
+    && apt-get install -y libreadline7 \
+    && apt-get install -y mysql-client
 
 COPY build/src/multiverse* /usr/local/bin/
 COPY entrypoint.sh /sbin/
 
 RUN mkdir /home/fnfn \
-&& mkdir /home/apps \
-#&& chmod 755 /usr/local/bin/multiverse \
-&& chmod 755 /sbin/entrypoint.sh 
-#&& ip -4 route list match 0/0 | awk '{print $3 "host.docker.internal"}' >> /etc/hosts
+    && chmod 755 /sbin/entrypoint.sh 
 
 VOLUME ["${HOME}"]
 EXPOSE 6815 6812
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,47 +1,9 @@
 #!/bin/bash
 
-# cp ${APPS_DIR}/multiverse /usr/local/bin/
-# cp ${APPS_DIR}/multiverse-cli /usr/local/bin/
-# cp ${APPS_DIR}/multiverse-dnseed /usr/local/bin/
-# cp ${APPS_DIR}/multiverse-miner /usr/local/bin/
-# cp ${APPS_DIR}/multiverse-server /usr/local/bin/
-# chmod 755 /usr/local/bin/multiverse
-
+service mysql stop
+service mysql start
 mysql -u root -P ${DB_PORT} -h ${DB_HOST} -e "grant all privileges on *.* to 'multiverse'@'%' identified by 'multiverse';"
 mysql -u root -P ${DB_PORT} -h ${DB_HOST} -e "flush privileges;"
 mysql -u root -P ${DB_PORT} -h ${DB_HOST} -e "create database if not exists ${DB_NAME};"
 
-# if [ -f "${HOME}/.multiverse/multiverse.conf" ]; then
-#     rm ${HOME}/.multiverse/multiverse.conf
-# fi
-
-# if [ ! -d "${HOME}/.multiverse/" ]; then
-#     mkdir ${HOME}/.multiverse/
-# fi
-
-# touch ${HOME}/.multiverse/multiverse.conf
-
-# if [ -n "${DNSEED}" ]; then
-#     echo "addnode=${DNSEED}" >> ${HOME}/.multiverse/multiverse.conf
-# fi
-# if [ -n "${DNSEED_PORT}" ]; then
-#     echo "port=${DNSEED_PORT}" >> ${HOME}/.multiverse/multiverse.conf
-# fi
-
-# echo "dbhost=${DB_HOST}" >> ${HOME}/.multiverse/multiverse.conf
-# echo "dbport=${DB_PORT}" >> ${HOME}/.multiverse/multiverse.conf
-# echo "dbname=${DB_NAME}" >> ${HOME}/.multiverse/multiverse.conf
-
-# if [ -n "${DBP_R_HOST}" ]; then
-#     echo "dbpparentnodeip=${DBP_R_HOST}" >> ${HOME}/.multiverse/multiverse.conf
-# fi
-
-# if [ -n "${DBP_R_PORT}" ]; then
-#     echo "dbpparentnodeport=${DBP_R_PORT}" >> ${HOME}/.multiverse/multiverse.conf
-# fi
-
-# if [ -n "${FORK_ID}" ]; then
-#     echo "supernodeforks=${FORK_ID}" >> ${HOME}/.multiverse/multiverse.conf
-# fi
-
-# multiverse
+tail -f /dev/null
