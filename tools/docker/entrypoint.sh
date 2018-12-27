19dd5143763548fcdb861f9d092bf4e0d70d9883
gc87 <sfme@qq.com>
2018-12-27 23:18:53 +0800

    [#]fix docker volume mapping error.

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ ! -d "${HOME}/.multiverse/" ]; then
+    mkdir -p ${HOME}/.multiverse/
+fi
+
+if [ ! -f "${HOME}/.multiverse/multiverse.conf" ]; then
+    cp /multiverse.conf ${HOME}/.multiverse/multiverse.conf
+fi
+
+chown -R mysql:mysql /var/lib/mysql
+mysqld --initialize-insecure --user=mysql
 service mysql stop
 service mysql start
 
@@ -7,4 +17,5 @@ mysql -e "grant all privileges on *.* to 'multiverse'@'%' identified by '${DB_PA
 mysql -e "flush privileges;"
 mysql -e "create database if not exists multiverse;"
 
-tail -f /dev/null
+#tail -f /dev/null
+exec "$@"
