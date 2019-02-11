c2666b7fdc3d1e3a24f618d05c89e6a5ef48661d
MathxH Chen <brainfvck@foxmail.com>
2019-02-11 17:34:40 +0800

    [+] Test: Add config comments for dnsmasq address options

--- test/ipv6_tests.cpp
+++ test/ipv6_tests.cpp
@@ -111,9 +111,12 @@ BOOST_AUTO_TEST_CASE ( dns_query )
     // configure dnsmasq: /etc/dnsmasq.conf (see conf path of file: --conf-dir --conf-file) 
     // OPTION address=/domain.com/[ipv4 | ipv6]
     // refers to http://www.thekelleys.org.uk/dnsmasq/docs/dnsmasq-man.html
+
+    // address=/ipv4-multiverse.com/127.0.0.1
     CNetHost v4Host("www.ipv4-multiverse.com",55);
     BOOST_CHECK(ResolveHost(v4Host) == "127.0.0.1");
 
+    // address=/ipv6-multiverse.com/2001:db8:0:23:8:800:200c:417a
     CNetHost v6Host("www.ipv6-multiverse.com",55);
     BOOST_CHECK(ResolveHost(v6Host) == "2001:db8:0:23:8:800:200c:417a");
 }
